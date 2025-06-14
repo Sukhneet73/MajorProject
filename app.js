91ae865f91ad34e50644287cb5c89ddbd6ed7fe5
Sukhneet73 <sukhneetbatth@gmail.com>
2025-06-14 19:05:45 +0530

    Add homepage route for Render live display

--- app.js
+++ app.js
@@ -128,6 +128,10 @@ app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
 
+app.get("/", (req, res) => {
+  res.send(" Major Project is live and working!");
+});
+
 app.use((err, req, res, next) => {
     let{statusCode=500,message="something went wrong "}=err;
     res.status(statusCode).render("error.ejs",{message});
