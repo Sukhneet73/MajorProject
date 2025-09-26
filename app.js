435eaa4e91d389cae15fd7d6c1e636da57f9b8bd
Sukhneet73 <sukhneetbatth@gmail.com>
2025-09-26 19:07:31 +0530

    Update scripts and port for Vercel deployment

--- app.js
+++ app.js
@@ -143,6 +143,7 @@ app.use((err, req, res, next) => {
 // console.log("server is listening to port 8080");
 // });
 
+const PORT = process.env.PORT || 8080;
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
 });
